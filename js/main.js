a928ab11f56ab13aa1528fef05c7a1ca8847b2da
Filip Skubacz <filip.skubacz.public@gmail.com>
2020-07-16 22:41:30 +0200

    Init JS project

---
+++ js/main.js
@@ -0,0 +1,4 @@
+import * as tf from "@tensorflow/tfjs-node";
+// or import * as tf from '@tensorflow/tfjs'
+// or import * as tf from '@tensorflow/tfjs-node-gpu'
+// See https://www.tensorflow.org/js/tutorials/setup
